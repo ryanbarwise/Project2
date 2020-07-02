27144b88064405be33c6d973ccb44dc5ffd9ddaf
Ryan Barwise <ryanbarwise@gmail.com>
2020-07-02 18:46:37 -0400

    api-routes

---
+++ models/movies.js
@@ -0,0 +1,67 @@
+module.exports = function(sequelize, DataTypes) {
+  // userMovie
+  /*
+  id | title | year | note | rating | userID
+  45 | titanic |1996 | too long, but underrated | 8/10 | 65
+  46 | titanic |1996 | Love anything with Dicaprio | 9/10 | 50
+  */
+
+  //first three are for movies from Aaron's api
+  var Movies = sequelize.define("UserMovies", {
+    // "UserMovies"
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1],
+      },
+    },
+
+    year: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      validate: {
+        len: [4],
+      },
+    },
+
+    genre: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+
+    userID: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+
+    userRating: {
+      type: DataTypes.INTEGER,
+      isNumeric: true,
+      max: 10,
+      min: 1,
+    },
+
+    watched: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+    },
+
+    notes: {
+      type: DataTypes.TEXT,
+      allowNull: true,
+    },
+
+    list: {
+      // watched, weekend-binge, save for rainy day list, etc
+      type: DataTypes.TEXT,
+      allowNull: true,
+    },
+
+    recommend: {
+      type: DataTypes.BOOLEAN,
+      allowNull: true,
+    },
+  });
+  return Movies;
+};
