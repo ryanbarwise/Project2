1408662671daf645f9e0bb43a1033465b4f45513
Demetrio <demetrioalmeida@gmail.com>
2020-07-02 23:31:18 -0400

    added handlebars file

--- public/js/home.js
+++ public/js/home.js
@@ -1,30 +1,9 @@
-$(document).ready(() => {
-
-    const omdb = require("../../omdb");
-
-    const textOne = $(".text1");
-    
-
-
-
-    const cityEl = $("<h1>");
-    const tempetureEl = $("<h5>");
-    const humidityEl = $("<h5>");
-    const windSpeedEl = $("<h5>");
-    const dateEl = $("<h5>");
-    cityEl.addClass("");
-    cityEl.attr("data-name");
-    cityEl.text(apiCityName);
-    dateEl.text(monthDayYear);
-    tempetureEl.text("Temperature: " + temperature);
-    humidityEl.text("Humidity: " + apiHumidity);
-    windSpeedEl.text("Wind speed: " + windSpeed);
-    // uvIndexEl.text(apiUvIndex);
-    // forecastEl.text(forecast);
-    $("#current-weather").append(cityEl);
-    $("#current-weather").append(dateEl);
-    $("#current-weather").append(tempetureEl);
-    $("#current-weather").append(humidityEl);
-    $("#current-weather").append(windSpeedEl);
-
-})
+// Aaron needs to tell me what route I'm going to do the GET request to / uses Axios in order to send data.
+$.ajax("/api/movies", {
+  type: "GET",
+}).then(
+  function(data) {
+    console.log(data);
+    // do something with the omdb
+  }
+);
--- routes/html-routes.js
+++ routes/html-routes.js
@@ -10,7 +10,7 @@ module.exports = function(app) {
     if (req.user) {
       res.redirect("/members");
     }
-    res.sendFile(path.join(__dirname, "../public/home.html"));
+    res.render("index", res);
   });
 
   app.get("/signup", (req, res) => {
